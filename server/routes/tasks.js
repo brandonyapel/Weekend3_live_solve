7c33bc38f3186f2ad1c271d01dd1bd82454f3482
Brandon Yapel <brandonyapel@gmail.com>
2017-11-27 10:12:50 -0600

    add tasks post route

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -28,5 +28,30 @@ router.get('/', function (req, res) {
     });
 });
 
+router.post('/', function(req,res){
+     // Attempt to connect to database
+     pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            // There was an error connecting to the database
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            // We connected to the database!!!
+            // Now, we're going to GET things from thd DB
+            client.query(`INSERT INTO tasks (name)
+                VALUES ($1);`, [req.body.name], function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    // Query failed. Did you test it in Postico?
+                    // Log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(201);
+                }
+            });
+        };
+    });
+});
 
 module.exports = router;
